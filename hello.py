4902f0ea145a09b25fce8280e34b3fe0ad6ff9be
akwjqkam <97821722+akwjqkam@users.noreply.github.com>
2022-01-17 02:50:30 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
-import tensorflow as tf
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import os
 
-hello = tf.constant('Hello World')
-sess = tf.Session()
+parser = reqparse.RequestParser()
+parser.add_argument('key')
 
-print(sess.run(hello))
+app = Flask(__name__)
+api = Api(app)
+
+class MyResource(Resource):
+    def get(self):
+        args = parser.parse_args()
+        return {'key': args['key']}
+
+api.add_resource(MyResource, '/api')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
